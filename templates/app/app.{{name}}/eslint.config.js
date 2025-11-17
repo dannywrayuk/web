ae877fa77a370c82d0a3ac86afc67e5592eac2ad
Danny Wray <48837241+dannywrayuk@users.noreply.github.com>
2025-11-17 04:59:00 +0000

    add app template

---
+++ templates/app/app.{{name}}/eslint.config.js
@@ -0,0 +1,1 @@
+export { default } from "@dannywrayuk/eslint/react";
---
+++ templates/app/app.{{name}}/src/routes/__root.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export const Route = createRootRoute({
+  component: RootComponent,
+});
+
+const queryClient = new QueryClient();
+
+function RootComponent() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <React.Fragment>
+        <Outlet />
+      </React.Fragment>
+    </QueryClientProvider>
+  );
+}
